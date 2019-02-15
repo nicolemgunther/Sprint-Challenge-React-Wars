1f2990d52eefa4d5b6bcc7b1ac6de16eac90df6f
Nicole Gunther <nicolemgunther@gmail.com>
2019-02-15 11:01:08 -0800

    added logos to header

--- starwars/src/components/SWHeader.js
+++ starwars/src/components/SWHeader.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import './StarWars.css';
+import rebelLogo from './img/rebelLogo.png';
+import empireLogo from './img/empireLogo.png';
 
-function Header() {
+function SWHeader() {
     return(
         <div className="Header">
+            <img src={rebelLogo} alt="rebel-logo" className="logo" />
             <h1>React Wars</h1>
+            <img src={empireLogo} alt="empire-logo" className="logo" />
         </div>
     )
 }
 
-export default Header;
+export default SWHeader;
